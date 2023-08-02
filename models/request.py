76eede920df2e5c5d4a846f19aa9a75a0477272e
Sourabh.Meena <sourabh.meena@FVFJ628CQ6L4.local>
2023-08-02 10:42:52 +0530

    request identation

--- models/request.py
+++ models/request.py
@@ -1,39 +1,40 @@
 from pydantic import BaseModel, validator, Field
-from pydantic_core import PydanticCustomError
+# from pydantic_core import PydanticCustomError
 from typing import Optional
 from sanic import json
 
 
 class ConvertCurrency(BaseModel):
-    from_ : list = Field(..., alias='from')
-    to : list
-    amount : Optional[list]
+    from_: list = Field(..., alias='from')
+    to: list
+    amount: Optional[list]
 
     @validator('from_')
-    def validate_from(cls,from_ ):
-        if len(from_[0]) != 3 :
+    def validate_from(cls, from_):
+        if len(from_[0]) != 3:
             raise ValueError(f"{from_} is not a Valid Currency")
-        
+
     @validator('to')
-    def validate_to(cls,to ):
-        if len(to[0]) != 3 :
+    def validate_to(cls, to):
+        if len(to[0]) != 3:
             raise ValueError(f"{to} is not a Valid Currency")
-    
+
+
 class ExchangeRates(BaseModel):
-    symbols : Optional[list]=['INR']
-    base : Optional[list]=['USD']
-    interval : Optional[list]=[1]
+    symbols: Optional[list] = ['INR']
+    base: Optional[list] = ['USD']
+    interval: Optional[list] = [1]
 
     @validator('base')
-    def validator_base(cls,base):
-        if len(base[0])!=3:
+    def validator_base(cls, base):
+        if len(base[0]) != 3:
             raise ValueError(f'{base[0]} is not a Valid Base Currency')
-    
+
     @validator('interval')
-    def validator_interval(cls,interval):
-        if(int(interval[0])<1 or int(interval[0]) > 60):
+    def validator_interval(cls, interval):
+        if (int(interval[0]) < 1 or int(interval[0]) > 60):
             raise ValueError('intervel should be between 1 to 60')
 
 
 class RemoveCurrencyModel(BaseModel):
-    currency : list
+    currency: list
